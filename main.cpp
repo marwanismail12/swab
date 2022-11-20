6f68cdf18e82fbb579c1e697c5db1a89f076329a
marwan <112703763+marwanismail12@users.noreply.github.com>
2022-11-20 23:04:29 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ float x,y,a;
+cout <<" Enter x: ";
+cin>>x;
+cout <<" Enter y: ";
+cin>>y;
+a=x;
+x=y;
+y=a;
+
+
+cout<<"x="<<x<<"\ny="<<y;
+
+
+    return 0;
+}
